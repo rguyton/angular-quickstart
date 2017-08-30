1f3f6cc02bacbf16d701c46061625d855604a9dd
Rick Guyton <rickguyton+github@gmail.com>
2017-08-29 21:29:58 -0500

    Angular tutorial progress. Adding search functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { AppRoutingModule }    from './app-routing.module';
 
@@ -29,6 +30,7 @@ import { AppRoutingModule }    from './app-routing.module';
                   AppComponent,
                   HeroDetailComponent,
                   HeroesComponent,
+                  HeroSearchComponent,
                   DashboardComponent
                 ],
   providers: [ HeroService ],
